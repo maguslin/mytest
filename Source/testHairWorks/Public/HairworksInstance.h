c4a3301dbac825a5de54861795e9e54c8bdc69d0
maguslin <lchen36@gmail.com>
2017-05-03 20:49:37 +0800

    增加instance

---
+++ Source/testHairWorks/Public/HairworksInstance.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "HairWorksInstance.generated.h"
+class UHairworksMaterial;
+class UHairworksAsset;
+
+USTRUCT(BlueprintType)
+struct  FHairWorksInstance
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** Assign HairWorks asset here. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Asset)
+	UHairworksAsset* Hair;
+
+	/** Whether override Hair Material of the HairWorks asset. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Asset)
+	bool bOverride = false;
+
+	/** A Hair Material to override the Hair Material of the HairWorks asset. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Instanced, Category = Hair)
+	UHairworksMaterial* HairMaterial;
+};
